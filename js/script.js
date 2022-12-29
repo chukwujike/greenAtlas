846a017e3b756b7e9f4ee06cf08382de1e2b8357
Ejeagwu Zikoha Derrick <zikoejeagwu@gmail.com>
2022-12-29 01:20:38 +0100

    Pricing section

--- js/script.js
+++ js/script.js
@@ -7,3 +7,33 @@ btn.addEventListener('click', () => {
     nav.classList.toggle('hidden')
     
 })
+
+const price = document.querySelector('.price');
+const price1 = document.querySelector('.price1');
+const price2 = document.querySelector('.price2');
+
+
+
+// bouncing animation for element with className price
+price.addEventListener('mouseover', () => {
+    price.children[3].className = 'animate-bounce font-sans font-bold mt-7 pl-5 pb-5';
+    setTimeout(() => {
+        price.children[3].className = 'font-sans font-bold mt-7 pl-5 pb-5'
+    }, 1500)
+})
+
+// bouncing animation for element with className price
+price1.addEventListener('mouseover', () => {
+    price1.children[3].className = 'animate-bounce font-sans font-bold mt-7 pl-5 pb-5';
+    setTimeout(() => {
+        price1.children[3].className = 'font-sans font-bold mt-7 pl-5 pb-5'
+    }, 1500)
+})
+
+// bouncing animation for element with className price
+price2.addEventListener('mouseover', () => {
+    price2.children[3].className = 'animate-bounce font-sans font-bold mt-7 pl-5 pb-5';
+    setTimeout(() => {
+        price2.children[3].className = 'font-sans font-bold mt-7 pl-5 pb-5'
+    }, 1500)
+})
