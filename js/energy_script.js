7eec54bcb8cef0d4649234edb4f132f021a4aaf5
chukwujike <chukwujike2000@gmail.com>
2023-01-02 19:07:52 +0100

    update

--- js/energy_script.js
+++ js/energy_script.js
@@ -13,3 +13,47 @@ var swiper = new Swiper(".slide-content", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+
+  document.addEventListener("DOMContentLoaded", () => {
+    // Get the select elements
+    const countrySelect = document.getElementById("countrySel");
+    const stateSelect = document.getElementById("stateSel");
+  
+    // Fetch the list of countries and states from the JSON file
+    fetch("https://gist.githubusercontent.com/ebaranov/41bf38fdb1a2cb19a781/raw/fb097a60427717b262d5058633590749f366bd80/gistfile1.json")
+      .then(res => res.json())
+      .then(data => {
+        // Populate the options for the country select element
+        let output = "";
+        data.countries.forEach(country => {
+          output += `<option value="${country.country}">${country.country}</option>`;
+        });
+        countrySelect.innerHTML = output;
+  
+        // Listen for the change event on the country select element
+        countrySelect.addEventListener("change", () => {
+          // Get the selected country
+          const selectedCountry = countrySelect.value;
+  
+          // Find the corresponding states for the selected country
+          const states = data.find(country => country.country === selectedCountry).states;
+  
+          // Populate the options for the state select element
+          let output = "";
+          states.forEach(state => {
+            output += `<option value="${state}">${state}</option>`;
+          });
+          stateSelect.innerHTML = output;
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+
+
+
+  
+
+  
